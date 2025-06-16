a16e9b0ceebe9ceb772d0a292962bd2856d0fa38
bocchi <boccfiap@outlook.com>
2025-06-16 13:23:26 -0300

    first commit

---
+++ src/main/java/dev/java10x/NinjaRegistration/NinjaProjectRegistrationApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.NinjaRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaProjectRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaProjectRegistrationApplication.class, args);
+	}
+
+}
